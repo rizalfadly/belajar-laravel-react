443f069492e05132e47a66834944fcd184f5c35b
fadli <rizalfadli31@gmail.com>
2023-02-13 09:27:44 +0700

    Add files via upload
    add and modif

---
+++ database/factories/newsFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\news>
+ */
+class newsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->title(),
+            'description' => fake()->paragraph(3, true),
+            'category' => fake()->name(),
+            'author' => fake()->email(),
+        ];
+    }
+}
